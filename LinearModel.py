e18872ccde99d1ffde5e09624d33a7e4a08ab059
Cheng Qiu <81718618+chengq220@users.noreply.github.com>
2022-12-30 20:51:03 -0500

    Add files via upload

--- LinearModel.py
+++ LinearModel.py
@@ -1,25 +1,23 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import max_error
 
 
 class LinearModel:
     def __init__(self, x):
         # of variables (thetas)
         self.theta = np.random.rand(x) #random weights initialization
-        self.alpha = 0.01              #learning rate
-
-        print(self.theta)
+        self.alpha = 0.1               #learning rate
+        self.regularization = 10       #regularization parameters
 
     #update the weights of the hypothesis
     def train(self,dataset):
         # Separate the independent and dependent variables and normalizing the dataset
         x = dataset.iloc[:, 0:-1]
-        x = x/np.abs(x.max())
+        x = (x-x.min())/(x.max()-x.min())
 
         y = dataset.iloc[:, -1]
-        y = y/np.abs(y.max())
+        y = (y-y.min())/(y.max()-y.min())
 
         #add the bias term to the linear regression model
         bias = np.ones_like(x.iloc[:,0]).reshape(len(x.iloc[:,0]),1)
@@ -44,25 +42,23 @@ class LinearModel:
 
     def train_model(self, x, y):
         m = len(x)
-        epoch = 25
+        epoch = 50
         for i in range(epoch): #running for 25 epochs
         #the loss function with MSE
             loss = np.power(np.subtract(self.predict(x),np.asarray(y)),2)
-            loss = np.sum(loss) / (2*m)
+            loss = np.sum(loss) / (2*m) + (self.regularization / 2 * m) * np.sum(x**2)
 
-            print("epoch ", i, " loss is ", loss)
+            #print("epoch ", i, " loss is ", loss)
 
             #update the weight by computing the gradient
             self.theta -= (self.alpha/m) * self.gradient_descent(x,y)
-        print(self.theta)
 
     def gradient_descent(self,x,y):
         gradient = np.zeros_like(self.theta)
         a = (np.subtract(self.predict(x), np.asarray(y))).tolist()
         for i in range(len(x.iloc[:,0])):
             b = x.iloc[i,:]
-            gradient += (a[i] * b)
-        #print(gradient)
+            gradient += (a[i] * b) + (self.regularization/len(x) * x.iloc[i,:])
         return gradient
 
     def predict(self,input):
@@ -70,11 +66,12 @@ class LinearModel:
         return prediction
 
     def modelAccuracy(self,xtest,ytest):
+        residue = 0
         y_pred = self.predict(xtest)
-        accuracy = max_error(ytest, y_pred)
-        print("Max error is: ", accuracy)
+        print("The average difference is: ", np.sum(np.abs(y_pred-ytest))/(2*len(y_pred)))
 
     def predict_discrete(self,input):
+        #print(self.theta)
         return np.matmul([1,input],np.transpose(self.theta))
 
     def __str__(self) -> str:
