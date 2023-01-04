26cb1f72ca43c62c7198daa3aa1e579aa5ca1f73
Cheng Qiu <81718618+chengq220@users.noreply.github.com>
2023-01-03 21:48:50 -0500

    Add files via upload

--- pytorch_implementaiton/torch_linear_regression.py
+++ pytorch_implementaiton/torch_linear_regression.py
@@ -1,9 +1,11 @@
 import torch
 import numpy as np
+from torchvision.transforms import ToTensor
+from torch.utils.data import TensorDataset, DataLoader
 from pytorch_implementaiton.LinearLineDataset import LinearLineDataset
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torch.autograd import Variable
+import pandas as pd
 
 class LinearRegression(torch.nn.Module):
     def __init__(self):
@@ -14,50 +16,53 @@ class LinearRegression(torch.nn.Module):
         return predict_y
 
 #manipulate the dataset to fit the requirements of pytorch
-linedataset = LinearLineDataset("../linear_line.csv")
-train_loader = DataLoader(linedataset,batch_size=5,shuffle=True)
+#dataset = LinearLineDataset("../linear_line.csv")
+#train_loader = DataLoader(dataset,batch_size=6,shuffle=True)
+
+#import the dataset
+dataset = pd.read_csv("../linear_line.csv")
+x_min,x_max = dataset.iloc[:,0].min(),dataset.iloc[:,0].max()
+x = (dataset.iloc[:,0] - x_min)/(x_max-x_min)
+x = x.to_numpy()
+y = dataset.iloc[:,1].to_numpy()
+
+#convert numpy to tensor
+x_t = torch.from_numpy(x).reshape(len(x),1).float()
+y_t = torch.from_numpy(y).reshape(len(y),1).float()
+
+#create the dataset loader
+tensor_dataset = TensorDataset(x_t, y_t)
+train_loader = DataLoader(tensor_dataset,batch_size=5,shuffle=True)
 
 #the linear model with one layer
-
 linear_model = LinearRegression()
 
 #the loss function --> Mean square error
 criterion = torch.nn.MSELoss(size_average=False)
-
-#The optimizer is the mini-batch gradient descent with learning rate of 0.1
-#calculate the gradient for each batch and find the mean and then update the weight
+#The optimizer is the stochastic gradient descent with learning rate of 0.1
 optimizer = torch.optim.SGD(linear_model.parameters(),lr=0.01)
 
-epoch = 15
+epoch = 300
 loss_value = []
 
 for i in range(epoch):
-    for batch in train_loader:
-        x = batch[:,0].reshape(-1,1)
-        y = batch[:,1].reshape(-1,1)
-
+    for x_b,y_b in train_loader:
         # compute the prediction
-        y_pred = linear_model(x)
-        print(y_pred)
+        y_pred = linear_model(x_b)
 
         # compute the loss function
-        loss = criterion(y, y_pred)
+        loss = criterion(y_pred, y_b)
         loss_value.append(loss.detach())
 
         # clear any gradient from the previous pass through gradient descent
         optimizer.zero_grad()
+
+        # perform back propogation and update the weights
         loss.backward()
         optimizer.step()
 
-#a line showing the loss function over each iteration of the batch gradient descent
-iteration = np.arange(0,len(loss_value))
-plt.figure()
-plt.plot(iteration,loss_value)
-plt.xlabel("iteration")
-plt.ylabel("loss")
+print(loss_value[len(loss_value)-1])
 
-plt.show()
-
-new_var = Variable(torch.Tensor([[100]]))
-pred_y = linear_model(new_var)
-print("predict (after training)", 100, pred_y.detach())
+#make predictions
+preds = linear_model(x_t)
+print(preds[100])
